74291a0d69c9922fa5b7e79e9a50496c27c39021
cpunicorns <42643184+cpunicorns@users.noreply.github.com>
2021-08-13 17:12:25 +0200

    small working version of the API

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"time"
@@ -11,39 +12,58 @@ import (
 )
 
 type Trick struct {
-	Id       int       `json:"id"`
+	Id       string    `json:"id"`
 	Name     string    `json:"name"`
-	Progress string    `json:"progess"`
+	Progress string    `json:"progress"`
 	State    string    `json:"state"`
 	Added_on time.Time `json:"added_on"`
 }
 
 type Tricks []Trick
 
+var AllTricks = []Trick{
+	{Id: "1", Name: "Sit", Progress: "learned", State: "done", Added_on: time.Now()},
+	{Id: "2", Name: "Lay down", Progress: "learned", State: "done", Added_on: time.Now()},
+}
+
 func listAllTricks(w http.ResponseWriter, r *http.Request) {
-	tricks := Tricks{
-		Trick{Id: 1, Name: "Sit", Progress: "learned", State: "done", Added_on: time.Now()},
-		Trick{Id: 2, Name: "Lay down", Progress: "learned", State: "done", Added_on: time.Now()},
+	json.NewEncoder(w).Encode(AllTricks)
+}
+
+func listOneTrick(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["id"]
+
+	for _, trick := range AllTricks {
+		if trick.Id == key {
+			json.NewEncoder(w).Encode(trick)
+		}
 	}
-	json.NewEncoder(w).Encode(tricks)
 }
 
 func insertTrick(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Test POST endpoint worked")
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	var trick Trick
+	json.Unmarshal(reqBody, &trick)
+	AllTricks = append(AllTricks, trick)
+
+	json.NewEncoder(w).Encode(trick)
 }
 
 func startPage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Start page hit!")
 }
 
-func handleRequests() {
+func HandleRequests() {
 	myRouter := mux.NewRouter().StrictSlash(true)
 	myRouter.HandleFunc("/", startPage)
 	myRouter.HandleFunc("/tricks", listAllTricks).Methods("GET")
 	myRouter.HandleFunc("/tricks", insertTrick).Methods("POST")
+	myRouter.HandleFunc("/tricks/{id}", listOneTrick).Methods("GET")
+	//myRouter.HandleFunc("/tricks/{id}", updateOneTrick).Methods("PUT")
 	log.Fatal(http.ListenAndServe(":8081", myRouter))
 }
 
 func main() {
-	handleRequests()
+	HandleRequests()
 }
